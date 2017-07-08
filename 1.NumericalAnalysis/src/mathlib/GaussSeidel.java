4bf3450cb69a5219dff27f273ee5a0c4010f17cc
Keeeeento <Keeeeento@users.noreply.github.com>
2017-07-08 15:41:19 +0900

    Add files via upload

--- 1.NumericalAnalysis/src/mathlib/GaussSeidel.java
+++ 1.NumericalAnalysis/src/mathlib/GaussSeidel.java
@@ -5,8 +5,40 @@ public class GaussSeidel {
 	static double epsilon = 1e-8;
 	static int maxCountNumber = 100000;
 
-	// ガウスザイデル法
 	public static Vector solve(Matrix a, Vector b) {
+		int n = b.getData().length;
+		int count;
+		Vector x = new Vector(n);
+		Vector xOld = new Vector(n);
+
+		for (count = 0; count < maxCountNumber; count++) {
+			for (int i = 0; i < n; i++) {
+				double sum = 0.0;
+				for (int j = 0; j < n; j++) {
+					if (j != i) {
+						sum += a.getData()[i][j] * x.getData()[j];
+					}
+				}
+
+				x.getData()[i] = (b.getData()[i] - sum) / a.getData()[i][i];
+			}
+			if (xOld.getRelativeErrorOfOneNorm(x) <= epsilon) {
+				break;
+			} else {
+				xOld = x.copy();
+			}
+		}
+		if (count < maxCountNumber) {
+			System.out.println("count = " + count);
+		} else {
+			System.out.println("収束しません");
+		}
+
+		return x;
+
+	}
+
+	public Vector solveUncompleted(Matrix a, Vector b) { // 未完成
 		int n = b.getData().length;
 		int count;
 		Vector x = new Vector(n);
--- 1.NumericalAnalysis/src/mathlib/Jacobi.java
+++ 1.NumericalAnalysis/src/mathlib/Jacobi.java
@@ -3,12 +3,41 @@ package mathlib;
 public class Jacobi {
 	static double epsilon = 10e-8;
 	static int maxCountNumber = 100000;
-	static int count = 0;
 
 	public static Vector solve(Matrix a, Vector b) {
 		int n = b.getData().length;
+		int count;
 		Vector x = new Vector(n);
 		Vector xOld = new Vector(n);
+
+		for (count = 0; count < maxCountNumber; count++) {
+			for (int i = 0; i < n; i++) {
+				x.getData()[i] = b.getData()[i];
+				for (int j = 0; j < n; j++) {
+					if (i != j) {
+						x.getData()[i] -= a.getData()[i][j] * xOld.getData()[j];
+					}
+				}
+				x.getData()[i] /= a.getData()[i][i];
+
+			}
+			// System.out.println(xOld.getRelativeErrorOfOneNorm(x));
+			if (xOld.getRelativeErrorOfOneNorm(x) <= epsilon) {
+				break;
+			} else {
+				xOld = x.copy();
+			}
+		}
+		System.out.println("count = " + count);
+
+		return x;
+	}
+
+	public static Vector solveWithSum(Matrix a, Vector b) {
+		int n = b.getData().length;
+		Vector x = new Vector(n);
+		Vector xOld = new Vector(n);
+		int count = 0;
 		for (count = 0; count < maxCountNumber; count++) {
 			for (int i = 0; i < n; i++) {
 				double sum = 0.0;
@@ -25,11 +54,13 @@ public class Jacobi {
 				xOld = x.copy();
 			}
 		}
-		System.out.println("count = " + count);
-
+		if (count < maxCountNumber) {
+			System.out.println("count = " + count);
+		} else {
+			System.out.println("収束しません");
+		}
 		// System.out.println(count);
 		return x;
 
 	}
-
 }
