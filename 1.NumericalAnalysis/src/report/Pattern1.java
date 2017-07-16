9f7d23dbbfc32d0074ae1861de56587d2a99a9fd
Keeeeento <Keeeeento@users.noreply.github.com>
2017-07-16 16:03:27 +0900

    Add files via upload

--- 1.NumericalAnalysis/src/report/Pattern1.java
+++ 1.NumericalAnalysis/src/report/Pattern1.java
@@ -1,19 +1,34 @@
 package report;
 
 import mathlib.GaussSeidel;
+import mathlib.GaussianElimination;
 import mathlib.Jacobi;
+import mathlib.LUDecomposition;
 import mathlib.Matrix;
 import mathlib.Vector;
 
 public class Pattern1 {
 	public static void main(String[] args) {
-		int n = 200;
+		int n = 1000;
+		int normNumber = 1;
 		Matrix a = Matrix.symmetricBand(n, 10, 2, 1);
-		a.printInt("a");
+		// a.printInt("a");
 		Vector xExact = Vector.allNumber(n, 2);
 		Vector b = a.multiply(xExact);
-		Jacobi.solveAndShowError(a, b, xExact, 1);
-		GaussSeidel.solveAndShowError(a, b, xExact, 1);
+
+		Jacobi.solveAndShowError(a, b, xExact, normNumber);
+
+		GaussSeidel.solveAndShowError(a, b, xExact, normNumber);
+
+		System.out.println("GaussianElimination");
+		System.out.println(GaussianElimination.solve(a, b).getRelativeError(normNumber, xExact));
+
+		System.out.println("LUDecomposition");
+		System.out.println(LUDecomposition.solve(a, b).getRelativeError(normNumber, xExact));
+
+		System.out.println("PivotGaussianElimination");
+		System.out.println(GaussianElimination.pivotSolve(a, b).getRelativeError(normNumber, xExact));
+
 	}
 
 }
